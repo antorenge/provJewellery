fec2fd4e210e9b9170c263e9daf5c21289cbf6a7
Antony Oroko <antoroko@gmail.com>
2018-05-10 18:59:33 +0300

    Add semantic ui

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
 import { fromPromise } from 'rxjs/observable/fromPromise';
-
 import { environment } from '../../environments/environment';
+import * as Web3 from 'web3';
+import * as contract from 'truffle-contract';
 
-const Web3 = require('web3');
-
-declare var window: any;
+declare let window: any;
 
 @Injectable()
 export class Web3Service {
@@ -21,19 +19,12 @@ export class Web3Service {
   checkAndInstantiateWeb3 = () => {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof window.web3 !== 'undefined') {
-      console.warn(
-        'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
-      );
       // Use Mist/MetaMask's provider
       this.web3 = new Web3(window.web3.currentProvider);
     } else {
-      console.warn(
-        'No web3 detected. Falling back to ${environment.HttpProvider}. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-      );
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      this.web3 = new Web3(
-        new Web3.providers.HttpProvider(environment.HttpProvider)
-      );
+      const provider = new Web3.providers.HttpProvider(environment.HttpProvider);
+      this.web3 = new Web3(provider);
     }
   };
 
