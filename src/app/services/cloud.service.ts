b57651b00c066be1b654c5ab2416c05a2c456fe6
Antony Oroko <antoroko@gmail.com>
2018-05-11 19:49:17 +0300

    Inject http client on cloud service

--- src/app/services/cloud.service.ts
+++ src/app/services/cloud.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CloudService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+
 
 }
