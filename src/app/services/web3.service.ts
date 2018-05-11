d08adf01aa16ea1bf4b88374f964443906e54ef3
Antony Oroko <antoroko@gmail.com>
2018-05-11 19:49:40 +0300

    Update services

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs/Observable';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import { environment } from '../../environments/environment';
 import * as Web3 from 'web3';
-import * as contract from 'truffle-contract';
 
 declare let window: any;
 
@@ -13,36 +12,32 @@ export class Web3Service {
   public web3: any;
 
   constructor() {
-    // this.checkAndInstantiateWeb3();
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.web3 !== 'undefined') {
+      // Use Mist/MetaMask's provider
+      this.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      const provider = new Web3.providers.HttpProvider(environment.HttpProvider);
+      this.web3 = new Web3(provider);
+    }
   }
 
-  // checkAndInstantiateWeb3 = () => {
-  //   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  //   if (typeof window.web3 !== 'undefined') {
-  //     // Use Mist/MetaMask's provider
-  //     this.web3 = new Web3(window.web3.currentProvider);
-  //   } else {
-  //     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  //     const provider = new Web3.providers.HttpProvider(environment.HttpProvider);
-  //     this.web3 = new Web3(provider);
-  //   }
-  // };
+  getAccounts(): Observable<any> {
+    return Observable.create(observer => {
+      this.web3.eth.getAccounts((err, accs) => {
+        if (err != null) {
+          observer.error('There was an error fetching your accounts.');
+        }
 
-  // getAccounts(): Observable<any> {
-  //   return Observable.create(observer => {
-  //     this.web3.eth.getAccounts((err, accs) => {
-  //       if (err != null) {
-  //         observer.error('There was an error fetching your accounts.')
-  //       }
+        if (accs.length === 0) {
+          observer.error('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
+        }
 
-  //       if (accs.length === 0) {
-  //         observer.error('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
-  //       }
-
-  //       observer.next(accs)
-  //       observer.complete()
-  //     });
-  //   })
-  // }
+        observer.next(accs);
+        observer.complete();
+      });
+    });
+  }
 
 }
