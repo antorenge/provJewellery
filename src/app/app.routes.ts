3833244dd7e7aab11419b64fa473c362dcaead30
Antony Oroko <antoroko@gmail.com>
2018-05-18 07:55:42 +0300

    Add production router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { HomeComponent } from './home/home.component';
 import { ProductDesignComponent } from './product-design/product-design.component';
+import { ArtisanProductionComponent } from './artisan-production/artisan-production.component';
 
 export const AppRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'product', component: ProductDesignComponent },
+    { path: 'production', component: ArtisanProductionComponent },
 
 ];
