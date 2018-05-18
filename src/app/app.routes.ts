57e77103d787c0ca262d8d50709035ae1df40329
Antony Oroko <antoroko@gmail.com>
2018-05-18 08:36:04 +0300

    Add validations route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { ProductDesignComponent } from './product-design/product-design.component';
 import { ArtisanProductionComponent } from './artisan-production/artisan-production.component';
 import { DeliveryComponent } from './delivery/delivery.component';
+import { ValidationComponent } from './validation/validation.component';
 
 export const AppRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'designs', component: ProductDesignComponent },
     { path: 'productions', component: ArtisanProductionComponent },
     { path: 'deliveries', component: DeliveryComponent },
+    { path: 'validations', component: ValidationComponent },
 
 ];
