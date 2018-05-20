df78cf37ce6610689edbdbbe46db4de70749a44d
Antony Oroko <antoroko@gmail.com>
2018-05-20 22:19:33 +0300

    Integrate ownership transfer to blockchain

--- src/app/ownership/ownership.component.ts
+++ src/app/ownership/ownership.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnershipService } from './services/ownership.service';
-import { OwnershipTransfer } from '../models';
+import { OwnershipTransfer, SignedObject } from '../models';
+import { Web3Service } from '../services/web3.service';
+import { ProvJewelleryService } from '../services/prov-jewellery.service';
+
 
 @Component({
   selector: 'app-ownership',
@@ -10,17 +13,47 @@ import { OwnershipTransfer } from '../models';
 export class OwnershipComponent implements OnInit {
 
   transfers: OwnershipTransfer[] = [];
+  account: any;
+  accounts: any;
 
-  constructor(private ownershipService: OwnershipService) { }
+  constructor(private ownershipService: OwnershipService,
+    private web3Service: Web3Service,
+    private provJewelleryService: ProvJewelleryService) { }
 
   ngOnInit() {
     this.ownershipService.getOwnershipTransfers().subscribe(data => {
       this.transfers = data;
     }, error => console.log(error));
+    // Get accounts
+    this.getInitialAccount();
   }
 
-  commitTransaction() {
+  getInitialAccount() {
+    this.web3Service.getAccounts().subscribe(accs => {
+      this.accounts = accs;
+      // Get first user account
+      this.account = this.accounts[0];
+    }, err => alert(err));
+  }
 
+  commitTransaction(transfer: OwnershipTransfer) {
+    // Get the signed delivery record
+    this.ownershipService.getSignedTransfer(transfer.id).subscribe(
+      (data: SignedObject) => {
+        console.log(data);
+        // Commit each delivered item (processed) to the blockchain network
+        transfer.items.forEach(item => {
+          this.setItemOwnership(item, data);
+        });
+      }, error => console.log(error));
+
+  }
+
+  setItemOwnership(item, data) {
+    this.provJewelleryService.setItemOwnership(item.serial_no, data.signed, this.account)
+      .subscribe(response => {
+        console.log(response);
+      }, error => console.log(error));
   }
 
 }
--- src/app/ownership/services/ownership.service.ts
+++ src/app/ownership/services/ownership.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { OwnershipTransfer } from '../../models';
+import { OwnershipTransfer, SignedObject } from '../../models';
 
 @Injectable()
 export class OwnershipService {
@@ -11,6 +11,11 @@ export class OwnershipService {
   getOwnershipTransfers() {
     const url = environment.baseUrl + 'payments/transfers/';
     return this.http.get<OwnershipTransfer[]>(url);
+  }
+
+  getSignedTransfer(id: string) {
+    const url = environment.baseUrl + 'payments/transfers/' + id + '/signed';
+    return this.http.get<SignedObject>(url);
   }
 
 }
