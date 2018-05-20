1f621cac0780ec4fed3e0432018bc4dadeeb4447
Antony Oroko <antoroko@gmail.com>
2018-05-20 16:02:19 +0300

    Remove unused import

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { fromPromise } from 'rxjs/observable/fromPromise';
 import { environment } from '../../environments/environment';
 import * as Web3 from 'web3';
 
