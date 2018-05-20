d8eccc4fe68d19a5df1b86247a496f8985a156ba
Antony Oroko <antoroko@gmail.com>
2018-05-20 18:50:18 +0300

    Add signed validation method

--- src/app/validation/services/validation.service.ts
+++ src/app/validation/services/validation.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Validation } from '../../models';
+import { Validation, SignedObject } from '../../models';
 
 @Injectable()
 export class ValidationService {
@@ -11,6 +11,11 @@ export class ValidationService {
   getValidations() {
     const url = environment.baseUrl + 'validations/qa-validations/';
     return this.http.get<Validation[]>(url);
+  }
+
+  getSignedValidation(id: string) {
+    const url = environment.baseUrl + 'validations/qa-validations/' + id + '/signed';
+    return this.http.get<SignedObject>(url);
   }
 
 }
